7f4d182ae46445e136cd712756b6e0caa6537984
Roger Sepúlveda <roger.planetgreen@gmail.com>
2014-12-07 08:17:12 -0500

    Added test for new class for running from command line

--- src/test/scala/com/huge/UISpec.scala
+++ src/test/scala/com/huge/UISpec.scala
@@ -3,11 +3,11 @@ package com.huge
 import org.scalatest._
 
 trait InterceptedIO extends IO {
-  var messages: StringBuffer = new StringBuffer()
+  private val messages: StringBuffer = new StringBuffer()
   def output: String = messages.toString
   
   override def print(message: String) = messages.append(s"${message}")
-  override def println(message: String) = print(s"{message}\n")
+  override def println(message: String) = print(s"${message}\n")
 }
 
 trait QuitCommand extends InterceptedIO {
@@ -29,5 +29,24 @@ class UISpec extends FlatSpec with Matchers {
     val ui = new UI with NoQuitCommand
     ui.run
     ui.output should not include ("program finished")
+  }
+
+  it should "run completely from a commands array" in {
+    val ui = new UIFromArguments(Array(
+        "C 20 4",
+        "L 1 2 6 2",
+        "L 6 3 6 4",
+        "R 16 1 20 3",
+        "B 10 3 o"
+      )) with InterceptedIO
+
+    ui.run
+    ui.output should include (
+      """----------------------
+        _|oooooooooooooooxxxxx|
+        _|xxxxxxooooooooox   x|
+        _|     xoooooooooxxxxx|
+        _|     xoooooooooooooo|
+        _----------------------""".stripMargin('_'))
   }
 }
